106ac8b1dc0c60bd1e09fed77a90682c10e6b418
cheo13 <cheis-bmx@hotmail.com>
2025-01-31 10:06:16 -0500

    json-config

--- src/main.ts
+++ src/main.ts
@@ -29,5 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT, '0.0.0.0');
+  console.log(`Server running on port ${process.env.PORT}`);
 }
 bootstrap();
